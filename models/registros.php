8f1eb2570bf04e0c68e4069eacd1ca18fe766e0a
Renato_O <renatorsdeoliveira@gmail.com>
2019-10-26 17:41:08 -0300

    Criado a verifivação para saber se usuário existe

--- models/registros.php
+++ models/registros.php
@@ -11,6 +11,41 @@
     $objeto = new db();
     $link = $objeto->conectando_banco();
 
+    //////////////////////////////////////////////////////////////////////////////
+    // verificar usuario 
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
+    if($resultado_id = mysqli_query($link, $sql)){
+        $dados_usuario = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+
+        if(isset($dados_usuario['usuario'])){
+            echo ' Usuário já cadastrado';
+        }
+        else{
+            echo ' Usuário não casdastrado, pode cadastrar';
+        }
+    }else{
+        echo 'erro ao tentar localizar o registro do usuário';
+    }
+    // verificar usuario 
+    // verificar email 
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    if($resultado_id = mysqli_query($link,$sql)){
+        $dados_usuario = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+
+        if($dados_usuario['email']){
+            echo ' E-mail já cadastrado';
+        }else{
+            echo ' E-mail não casdastrado, pode cadastrar';
+
+        }
+    }else{
+        echo 'erro ao tentar localizar o registro do usuário';
+    }
+    // verificar email 
+    //////////////////////////////////////////////////////////////////////////////
+
+    die();
+
     $sql = "insert into usuarios(usuario, email, senha) values('$usuario', '$email', '$senha')";
 
     if(mysqli_query($link, $sql)){

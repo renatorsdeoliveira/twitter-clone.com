124c2b84fac61af5199a528a6ab7c6c66b5d9df7
Renato_O <renatorsdeoliveira@gmail.com>
2019-10-31 11:49:32 -0300

    Separando os scripts

--- home.php
+++ home.php
@@ -87,6 +87,7 @@
 	    </div>
 	
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.js"></script>
+		<script src="javascript/home.js"></script>
 		<script src="javascript/script.js?Zfsssaf"></script>
 	
 	</body>
